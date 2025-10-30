9b50163f0110b1778c3868b69aa985b9daf49a9d
Nazarii Kuntsevych <kuntsevych.n.v@gmail.com>
2025-10-30 21:06:15 +0200

    feat: add auth module

--- src/main.py
+++ src/main.py
@@ -1,10 +1,11 @@
 from fastapi import FastAPI
 
 from .api.exception_handelrs import not_found_handler
-from .api.routes import user_router
+from .api.routes import auth_router, user_router
 from .config import settings
 from .repositories import NotFound
 
 app = FastAPI(debug=settings.DEBUG)
+app.include_router(auth_router)
 app.include_router(user_router)
 app.add_exception_handler(NotFound, not_found_handler)
